f96685850d156b63254062e065dbf3761f5f8463
nvmnghia <nvmnghia@gmail.com>
2022-03-07 12:56:08 +0700

    refactor: move functions outside `<Counter>`

--- src/counter/hook.js
+++ src/counter/hook.js
@@ -8,55 +8,72 @@ const Status = {
   FAILED: 'FAILED',
 };
 
+/* Effects */
+
+const initialLoad = (setCounter) =>
+  (async () => {
+    try {
+      setCounter({ value: await API.get(), status: Status.SUCCESS });
+    } catch {
+      setCounter({ value: undefined, status: Status.FAILED });
+    }
+  })();
+
+// Revert to IDLE 2s after SUCCESS
+const idleAfterSuccess = (counter, setCounter) => {
+  if (counter.status === Status.SUCCESS) {
+    const timerID = setTimeout(
+      () => setCounter({ value: counter.value, status: Status.IDLE }),
+      2000
+    );
+    return () => clearTimeout(timerID);
+  }
+};
+
+/* Callbacks */
+
+const _decrement = (value, setCounter) => async () => {
+  setCounter({ value, status: Status.LOADING });
+
+  // TODO: setState() batching INSIDE Promise is only available in 18+ WITH createRoot()
+  try {
+    setCounter({ value: await API.decrement(), status: Status.SUCCESS });
+  } catch {
+    setCounter({ value, status: Status.FAILED });
+  }
+};
+
+const _increment = (value, setCounter) => async () => {
+  setCounter({ value, status: Status.LOADING });
+
+  try {
+    setCounter({ value: await API.increment(), status: Status.SUCCESS });
+  } catch {
+    setCounter({ value, status: Status.FAILED });
+  }
+};
+
+/* Component */
+
 const useNghiaCounter = () => {
   const [counter, setCounter] = useState({
     value: undefined,
     status: Status.LOADING,
   });
 
-  // Initial load
-  useEffect(() => {
-    (async () => {
-      try {
-        setCounter({ value: await API.get(), status: Status.SUCCESS });
-      } catch {
-        setCounter({ value: undefined, status: Status.FAILED });
-      }
-    })();
-  }, []);
+  useEffect(() => initialLoad(setCounter), []);
 
-  // Revert to IDLE after 2s
-  useEffect(() => {
-    if (counter.status === Status.SUCCESS) {
-      const timerID = setTimeout(
-        () => setCounter({ value: counter.value, status: Status.IDLE }),
-        2000
-      );
+  useEffect(() => idleAfterSuccess(counter, setCounter), [counter]);
 
-      return () => clearTimeout(timerID);
-    }
-  }, [counter]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const decrement = useCallback(_decrement(counter.value, setCounter), [
+    counter.value,
+  ]); // EXACTLY counter.value, NOT the whole counter, which is created repeatedly
 
-  // TODO: setState() batching INSIDE Promise is only available in 18+ WITH createRoot()
-  const decrement = useCallback(async () => {
-    setCounter({ value: counter.value, status: Status.LOADING });
-
-    try {
-      setCounter({ value: await API.decrement(), status: Status.SUCCESS });
-    } catch {
-      setCounter({ value: counter.value, status: Status.FAILED });
-    }
-  }, [counter.value]); // EXACTLY counter.value, NOT the whole counter
-
-  const increment = useCallback(async () => {
-    setCounter({ value: counter.value, status: Status.LOADING });
-
-    try {
-      setCounter({ value: await API.increment(), status: Status.SUCCESS });
-    } catch {
-      setCounter({ value: counter.value, status: Status.FAILED });
-    }
-  }, [counter.value]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const increment = useCallback(_increment(counter.value, setCounter), [
+    counter.value,
+  ]);
 
   return [counter, increment, decrement];
 };
