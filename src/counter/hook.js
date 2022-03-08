059976ae7a9654111bb1bd313f6d168a366e34de
nvmnghia <nvmnghia@gmail.com>
2022-03-08 17:29:51 +0700

    refactor: move functions into hook

--- src/counter/hook.js
+++ src/counter/hook.js
@@ -7,61 +7,42 @@ const Status = {
   FAILED: 'FAILED',
 };
 
-/* Effects */
-
-const initialLoad = (setCounter) =>
-  (async () => {
-    try {
-      setCounter({ value: await API.get(), status: Status.SUCCESS });
-    } catch {
-      setCounter({ value: undefined, status: Status.FAILED });
-    }
-  })();
-
-/* Callbacks */
-
-const _decrement = async (value, setCounter) => {
-  setCounter({ value, status: Status.LOADING });
-
-  // TODO: setState() batching INSIDE Promise is only available in 18+ WITH createRoot()
-  try {
-    setCounter({ value: await API.decrement(), status: Status.SUCCESS });
-  } catch {
-    setCounter({ value, status: Status.FAILED });
-  }
-};
-
-const _increment = async (value, setCounter) => {
-  setCounter({ value, status: Status.LOADING });
-
-  try {
-    setCounter({ value: await API.increment(), status: Status.SUCCESS });
-  } catch {
-    setCounter({ value, status: Status.FAILED });
-  }
-};
-
-/* Custom hook */
-
 const useNghiaCounter = () => {
   const [counter, setCounter] = useState({
     value: undefined,
     status: Status.LOADING,
   });
 
-  useEffect(() => initialLoad(setCounter), []);
+  useEffect(() => {
+    (async () => {
+      try {
+        setCounter({ value: await API.get(), status: Status.SUCCESS });
+      } catch {
+        setCounter({ value: undefined, status: Status.FAILED });
+      }
+    })();
+  }, []);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const decrement = useCallback(
-    () => _decrement(counter.value, setCounter),
-    [counter.value]
-  ); // EXACTLY counter.value, NOT the whole counter, which is created repeatedly
+  const decrement = useCallback(async () => {
+    setCounter({ value: counter.value, status: Status.LOADING });
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const increment = useCallback(
-    () => _increment(counter.value, setCounter),
-    [counter.value]
-  );
+    // TODO: setState() batching INSIDE Promise is only available in 18+ WITH createRoot()
+    try {
+      setCounter({ value: await API.decrement(), status: Status.SUCCESS });
+    } catch {
+      setCounter({ value: counter.value, status: Status.FAILED });
+    }
+  }, [counter.value]); // EXACTLY counter.value, NOT the whole counter, which is created repeatedly
+
+  const increment = useCallback(async () => {
+    setCounter({ value: counter.value, status: Status.LOADING });
+
+    try {
+      setCounter({ value: await API.increment(), status: Status.SUCCESS });
+    } catch {
+      setCounter({ value: counter.value, status: Status.FAILED });
+    }
+  }, [counter.value]);
 
   return [counter, increment, decrement];
 };
